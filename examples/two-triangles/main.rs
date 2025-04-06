4275e84144749bf6d0e7fe7969d623e175e567aa
Archie Mourad <mouradarchie+github@gmail.com>
2025-04-06 11:23:02 +1000

    'two-triangles': refactor example for clarity

--- examples/two-triangles/main.rs
+++ examples/two-triangles/main.rs
@@ -12,19 +12,16 @@ fn main() {
         .create_window("Triangle Window 2", 800, 600)
         .expect("Failed to create window 2");
 
-    let pipeline = Arc::new({
-        let window = app.window_mut(window1_id).expect("Failed to get window 1");
+    let window1 = app.window_mut(window1_id).expect("Failed to get window 1");
 
-        window.create_pipeline(wgpu::include_wgsl!("shader.wgsl"), "vs_main", "fs_main")
-    });
+    let pipeline =
+        Arc::new(window1.create_pipeline(wgpu::include_wgsl!("shader.wgsl"), "vs_main", "fs_main"));
 
-    if let Some(window) = app.window_mut(window1_id) {
-        window.set_pipeline(pipeline.clone());
-    }
+    window1.set_pipeline(pipeline.clone());
 
-    if let Some(window) = app.window_mut(window2_id) {
-        window.set_pipeline(pipeline.clone());
-    }
+    app.window_mut(window2_id)
+        .expect("Failed to get window 2")
+        .set_pipeline(pipeline.clone());
 
     app.run(ControlFlow::Poll, None).expect("Failed to run app");
 }
